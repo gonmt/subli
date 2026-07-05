e0c23af15436689fe4f82aa1e377676af4a4a88a
Gonzalo Torres <torresgonzalom@gmail.com>
2026-07-05 16:25:54 -0300

    fix ci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,9 +10,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'test';
 putenv('APP_ENV=test');
 
-if (file_exists(dirname(__DIR__) . '/.env')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
-}
+(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 
 if ($_SERVER['APP_DEBUG'] ?? false) {
     umask(0000);
